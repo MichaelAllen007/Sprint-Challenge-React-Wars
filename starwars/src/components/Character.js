9f9776d333a6070f974637b0b0b784fbcda5225a
unknown <allenmk20073@yahoo.com>
2020-05-31 22:52:48 -0400

    Finished. Questions answered.

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,32 @@
 // Write your Character component here
+import React, {useState} from 'react';
+import styled from 'styled-components';
+
+export default function Character(props) {
+    
+    const name = props.name;
+    const height = props.height;
+    const mass = props.mass;
+    const birthYear = props.birthYear;
+
+    const CharacterCard = styled.div`
+    background-color: white;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    color: black;
+    font-family: monospace;
+    margin: 5%;
+    padding: 5%;
+    width: 300px;
+    `;
+
+    return (
+        <CharacterCard>
+            <h2>Name: {name}</h2>
+            <p>Birth Year: {birthYear}</p>
+            <p>Height: {height}</p>
+            <p>Mass: {mass}</p>
+        </CharacterCard>
+    );
+};
